fa75ed06f6faa251dde4127bd3c6c119354b21fa
ruchira206 <ruchirakamble26@gmail.com>
2026-08-08 22:48:37 +0530

    Add JWT auth middleware and protect DELETE /books/:id route

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const bookController = require('../controllers/bookController');
+const protect = require('../middleware/authMiddleware');
 router.post('/', bookController.createBook);
 router.get('/', bookController.getBooks);
 router.get('/:id', bookController.getBookById);
 router.put('/:id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.delete('/:id', protect, bookController.deleteBook);
 module.exports = router;
